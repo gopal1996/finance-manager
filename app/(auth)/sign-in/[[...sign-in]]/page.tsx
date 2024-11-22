8645b42fbb5a14fe9bd6850d134c484e88a77ad9
Gopalakrishnan <krishnan.gopal34@gmail.com>
2024-11-22 14:49:43 +0530

    fix: :art: initial commit

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import { SignIn, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
+import { Loader2 } from "lucide-react";
 
-const SignInPage = () => {
-  return <div>SignInPage</div>;
-};
-
-export default SignInPage;
+export default function Page() {
+  return (
+    <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
+      <div className="h-full flex flex-col items-center justify-center px-4">
+        <ClerkLoaded>
+          <SignIn />
+        </ClerkLoaded>
+        <ClerkLoading>
+          <Loader2 className="animate-spin text-muted-foreground" />
+        </ClerkLoading>
+      </div>
+    </div>
+  );
+}
